9892fcd194c6d448fc98ebf0908ff7e943fb0e10
ArsMFad <arsmfad@gmail.com>
2018-06-17 12:24:03 +0300

    It will be fun

---
+++ security/scanports.py
@@ -0,0 +1,40 @@
+import socket
+
+
+def scan():
+    host = input("HOST--->")
+    if not host:
+        print("Error: must be input")
+        raise SystemExit
+
+    try:
+        ports = list(map(int, input("PORTS--->").split()))
+    except ValueError:
+        print("Error: must be integer")
+        raise SystemExit
+
+    if not ports:
+        print("Error: must be input")
+        raise SystemExit
+
+    for some in ports:
+        if int(some) > 65535:
+            print("Error: port must be 0-65535")
+            raise SystemExit
+
+    print("\n")
+    print("~" * 50)
+
+    for port in ports:
+        try:
+            scan = socket.socket()
+            scan.settimeout(1)
+            scan.connect((host, port))
+        except socket.error:
+            print("PORT--->", port, "[CLOSED]")
+        else:
+            print("PORT--->", port, "[OPEN]")
+    print("~" * 50)
+
+
+scan()
