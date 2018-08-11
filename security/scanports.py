d514e51ae520832b6dbf91828b813a3cd144b981
ArsMFad <arsmfad@gmail.com>
2018-08-11 20:59:08 +0300

    It is colored

--- security/scanports.py
+++ security/scanports.py
@@ -1,42 +1,46 @@
 import socket
+from termcolor import cprint
 
+from colorama import Fore
+import colorama
+
+colorama.init()
 
 def scan():
-    host = input("HOST--->")
-    if not host:
-        print("Error: must be input")
-        raise SystemExit
+	host = input("HOST--->")
+	if not host:
+		print(Fore.RED, "Error: must be input")
+		raise SystemExit
 
-    try:
-        ports = list(map(int, input("PORTS--->").split()))
-    except ValueError:
-        print("Error: must be integer")
-        raise SystemExit
+	try:
+		ports = list(map(int, input("PORTS--->").split()))
+	except ValueError:
+		print(Fore.RED, "Error: must be integer")
+		raise SystemExit
 
-    if not ports:
-        print("Error: must be input")
-        raise SystemExit
+	if not ports:
+		print(Fore.RED, "Error: must be input")
+		raise SystemExit
 
-    for some in ports:
-        if int(some) > 65535:
-            print("Error: port must be 0-65535")
-            raise SystemExit
+	for some in ports:
+		if int(some) > 65535:
+			print(Fore.RED, "Error: port must be 0-65535")
+			raise SystemExit
 
-    print("\n")
-    print("~" * 50)
+	print("\n")
+	print("~"*50)
 
-    for port in ports:
-        try:
-            scan = socket.socket()
-            scan.settimeout(1)
-            scan.connect((host, port))
-        except socket.error:
-            print("PORT--->", port, "[CLOSED]")
-        else:
-            print("PORT--->", port, "[OPEN]")
-    print("~" * 50)
-
+	for port in ports:
+		try:
+			scan = socket.socket()
+			scan.settimeout(1)
+			scan.connect((host, port))
+		except socket.error:
+			print(Fore.RED, "PORT--->", port, "[CLOSED]" + Fore.RESET)
+		else:
+			print(Fore.GREEN, "PORT--->", port, "[OPEN]" + Fore.RESET)
+	print("~"*50)
+	
+	scan.close()
 
 scan()
-
-#Use it :3
