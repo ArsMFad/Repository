0bde53fc97d65ba8a3ef97ba31c2382b7751fa27
Arsenii Fadeev <arsmfad@gmail.com>
2019-12-18 20:30:31 +0300

    Just made it faster

--- tasks_from_school/strings_task_16.cpp
+++ tasks_from_school/strings_task_16.cpp
@@ -44,8 +44,10 @@ vector<string> stov (string s) {
 }
 
 int main() {
+  ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+
   string arr;
   getline(cin, arr);
   arr.erase(arr.size() - 1, 1);
